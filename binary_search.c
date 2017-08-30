16001f23137a996de0b240b4f1620ed16b91a607
ArchKudo <vgd.vyas17@gmail.com>
2017-08-30 16:50:40 +0530

    Use strict int types

--- binary_search.c
+++ binary_search.c
@@ -1,14 +1,15 @@
 #include "binary_search.h"
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-int binary_search(int *arr, int size, int query) {
-  int max = size - 1;
-  int min = 0;
+int16_t binary_search(int_fast64_t *arr, uint8_t size, int_fast64_t query) {
+  uint8_t max = size - 1;
+  uint8_t min = 0;
 
   while (min <= max) {
-    int mid = (int)((max + min) / 2);
-    int val = arr[mid];
+    uint8_t mid = (uint8_t)(min + (max - min) / 2);
+    int_fast64_t val = arr[mid];
     if (val == query) {
       return mid;
     } else if (val < query) {
--- binary_search.h
+++ binary_search.h
@@ -1,13 +1,16 @@
 #ifndef __BINARY_SEARCH__
 #define __BINARY_SEARCH__
 
+#include <stdint.h>
+
 /**
  * Binary search implementation, searches for `query` in `arr`
  * @param  arr  The array in which the `query` is searched for
  * @param  size The size of the array `arr`
  * @param  query  The element to search for in the array `arr`
- * @return      Returns index of `arr` at which `query` if exists is found else -1
+ * @return      Returns index of `arr` at which `query` is found
+ *              if exists, else -1
  */
-int binary_search(int *arr, int size, int query);
+int16_t binary_search(int_fast64_t *arr, uint8_t size, int_fast64_t query);
 
 #endif
