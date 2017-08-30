8d6f474ad73883d3bd08792f5b7848ac32ae7f7b
ArchKudo <vgd.vyas17@gmail.com>
2017-08-30 05:58:26 +0530

    Added include guards

--- binary_search.h
+++ binary_search.h
@@ -1,3 +1,6 @@
+#ifndef __BINARY_SEARCH__
+#define __BINARY_SEARCH__
+
 /**
  * Binary search implementation, searches for `pin` in `hay`
  * @param  hay  The array in which the `pin` is searched for
@@ -6,3 +9,5 @@
  * @return      Returns index of `hay` at which `pin` if exists is found else -1
  */
 int binary_search(int *hay, int size, int pin);
+
+#endif
